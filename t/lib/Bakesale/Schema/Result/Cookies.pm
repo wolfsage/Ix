9d72d133e37ef0d3ab9da0038c687b018802efb8
Ricardo Signes <rjbs@cpan.org>
2016-02-16 17:41:29 +0000

    table for state next numbers

--- t/lib/Bakesale/Schema/Result/Cookies.pm
+++ t/lib/Bakesale/Schema/Result/Cookies.pm
@@ -2,8 +2,10 @@ package Bakesale::Schema::Result::Cookies;
 use base qw/DBIx::Class::Core/;
 
 __PACKAGE__->load_components(qw/InflateColumn::DateTime/); # for example
+
 __PACKAGE__->table('cookies');
 
-__PACKAGE__->add_columns(qw/ id accountid state type baked_at /);
+__PACKAGE__->add_columns(qw( id accountid state type baked_at ));
+__PACKAGE__->set_primary_key('id');
 
 1;
