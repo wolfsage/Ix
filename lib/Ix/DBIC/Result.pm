2cd3d69992108a621671d1e76d573731a446cb28
Ricardo Signes <rjbs@cpan.org>
2016-02-21 03:40:15 +0000

    make the state-keeping columns generic

--- lib/Ix/DBIC/Result.pm
+++ lib/Ix/DBIC/Result.pm
@@ -4,11 +4,21 @@ package Ix::DBIC::Result;
 
 use parent 'DBIx::Class';
 
+use experimental qw(signatures postderef);
+
 sub ix_type_key { Carp::confess("ix_type_key not implemented") }
 
 # XXX This should probably instead be Rx to validate the user properites.
 sub ix_user_property_names { return () };
 
 sub ix_default_properties { return {} }
+
+sub ix_add_columns ($class) {
+  $class->add_columns(
+    id          => { data_type => 'integer', is_auto_increment => 1 },
+    accountId   => { is_nullable => 0 },
+    state       => { data_type => 'ingeger', is_nullable => 0 },
+  );
+}
 
 1;
--- t/lib/Bakesale/Schema/Result/Cookies.pm
+++ t/lib/Bakesale/Schema/Result/Cookies.pm
@@ -5,10 +5,9 @@ __PACKAGE__->load_components(qw/+Ix::DBIC::Result/); # for example
 
 __PACKAGE__->table('cookies');
 
+__PACKAGE__->ix_add_columns;
+
 __PACKAGE__->add_columns(
-  id         => { data_type => 'integer', is_auto_increment => 1 },
-  accountId  => { is_nullable => 0 },
-  state      => { data_type => 'ingeger', is_nullable => 0 },
   type       => { is_nullable => 0 },
   baked_at   => { data_type => 'datetime', is_nullable => 0 },
 );
