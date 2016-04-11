5988474388dfd145e69e39c72b38e04722c2ed66
Ricardo Signes <rjbs@cpan.org>
2016-04-11 15:53:59 -0400

    fix where road is loaded, to make MOP happy

--- t/lib/Bakesale/App.pm
+++ t/lib/Bakesale/App.pm
@@ -5,18 +5,19 @@ use experimental qw(signatures postderef);
 
 use Bakesale;
 
-with 'Ix::App';
-
 use JSON;
 
 use namespace::autoclean;
 
 has connect_info => (
+  is      => 'ro',
   lazy    => 1,
   default => sub {
     my $info = Bakesale::Test->test_schema_connect_info;
   },
 );
+
+with 'Ix::App';
 
 has '+processor' => (default => sub { Bakesale->new });
 
