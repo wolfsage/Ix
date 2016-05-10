985aa9a21eb71098e98f4753d1f62f0d4645f00c
Ricardo Signes <rjbs@cpan.org>
2016-05-10 16:21:45 -0400

    merge the Processor roles
    we were never going to use non-DBIC ones, I think

--- lib/Ix/Processor.pm
+++ lib/Ix/Processor.pm
@@ -11,6 +11,62 @@ use namespace::autoclean;
 
 requires 'handler_for';
 
+around handler_for => sub ($orig, $self, $method, @rest) {
+  my $handler = $self->$orig($method, @rest);
+  return $handler if $handler;
+
+  my $h = $self->_dbic_handlers;
+  return $h->{$method} if exists $h->{$method};
+
+  return;
+};
+
+requires 'schema_class';
+
+sub context_class { 'Ix::Context' }
+
+sub get_context ($self, $arg) {
+  Ix::Context->new({
+    accountId => $arg->{accountId},
+    schema    => $self->schema_class->connect($arg->{connect_info}->@*),
+    processor => $self,
+  });
+}
+
+has _dbic_handlers => (
+  is   => 'ro',
+  lazy => 1,
+  init_arg => undef,
+  default => sub {
+    my ($self) = @_;
+
+    my %handler;
+
+    my $source_reg = $self->schema_class->source_registrations;
+    for my $moniker (keys %$source_reg) {
+      my $rclass = $source_reg->{$moniker}->result_class;
+      next unless $rclass->isa('Ix::DBIC::Result');
+      my $key  = $rclass->ix_type_key;
+      my $key1 = $rclass->ix_type_key_singular;
+
+      $handler{"get\u$key"} = sub ($self, $ctx, $arg = {}) {
+        $ctx->schema->resultset($moniker)->ix_get($ctx, $arg);
+      };
+
+      $handler{"get\u${key1}Updates"} = sub ($self, $ctx, $arg = {}) {
+        $ctx->schema->resultset($moniker)->ix_get_updates($ctx, $arg);
+      };
+
+      $handler{"set\u$key"} = sub ($self, $ctx, $arg) {
+        $ctx->schema->resultset($moniker)->ix_set($ctx, $arg);
+      };
+
+    }
+
+    return \%handler;
+  }
+);
+
 # It is tempting to wrap this in a transaction.  Consider the case where a
 # method does not return an Ix::Result, so we can't map it into the final
 # result set.  That's going to throw an exception, now.  We could catch it and
