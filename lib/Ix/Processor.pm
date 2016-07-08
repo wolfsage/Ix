46bb3fce70827cf75636f86b8604489e246983e8
Ricardo Signes <rjbs@cpan.org>
2016-07-08 16:21:30 -0400

    use txn_do instead of begin/commit, to ease upcoming refactor

--- lib/Ix/Processor.pm
+++ lib/Ix/Processor.pm
@@ -85,58 +85,56 @@ has _dbic_handlers => (
 sub process_request ($self, $ctx, $calls) {
   my @results;
 
-  $ctx->schema->txn_begin;
+  $ctx->schema->txn_do(sub {
+    CALL: for my $call (@$calls) {
+      # On one hand, I am tempted to disallow ambiguous cids here.  On the other
+      # hand, the spec does not. -- rjbs, 2016-02-11
+      my ($method, $arg, $cid) = @$call;
 
-  CALL: for my $call (@$calls) {
-    # On one hand, I am tempted to disallow ambiguous cids here.  On the other
-    # hand, the spec does not. -- rjbs, 2016-02-11
-    my ($method, $arg, $cid) = @$call;
+      my $handler = $self->handler_for( $method );
 
-    my $handler = $self->handler_for( $method );
+      unless ($handler) {
+        push @results, [ error => { type => 'unknownMethod' }, $cid ];
+        next CALL;
+      }
 
-    unless ($handler) {
-      push @results, [ error => { type => 'unknownMethod' }, $cid ];
-      next CALL;
+      my @rv = try {
+        $self->$handler($ctx, $arg);
+      } catch {
+        if ($_->$_DOES('Ix::Error')) {
+          return $_;
+        } else {
+          warn $_;
+          die $_;
+        }
+      };
+
+      RV: for my $i (0 .. $#rv) {
+        local $_ = $rv[$i];
+        push @results, $_->$_DOES('Ix::Result')
+                     ? [ $_->result_type, $_->result_properties, $cid ]
+                     : [ error => 'garbledResponse', $cid ];
+
+        if ($results[-1][0] eq 'error' && $i < $#rv) {
+          # In this branch, we have a potential return value like:
+          # (
+          #   [ valid => ... ],
+          #   [ error => ... ],
+          #   [ valid => ... ],
+          # );
+          #
+          # According to the JMAP specification ("§ Errors"), we shouldn't be
+          # getting anything after the error.  So, remove it, but also file an
+          # exception report. -- rjbs, 2016-02-11
+          #
+          # TODO: file internal error report -- rjbs, 2016-02-11
+          last RV;
+        }
+      }
     }
 
-    my @rv = try {
-      $self->$handler($ctx, $arg);
-    } catch {
-      if ($_->$_DOES('Ix::Error')) {
-        return $_;
-      } else {
-        warn $_;
-        die $_;
-      }
-    };
-
-    RV: for my $i (0 .. $#rv) {
-      local $_ = $rv[$i];
-      push @results, $_->$_DOES('Ix::Result')
-                   ? [ $_->result_type, $_->result_properties, $cid ]
-                   : [ error => 'garbledResponse', $cid ];
-
-      if ($results[-1][0] eq 'error' && $i < $#rv) {
-        # In this branch, we have a potential return value like:
-        # (
-        #   [ valid => ... ],
-        #   [ error => ... ],
-        #   [ valid => ... ],
-        # );
-        #
-        # According to the JMAP specification ("§ Errors"), we shouldn't be
-        # getting anything after the error.  So, remove it, but also file an
-        # exception report. -- rjbs, 2016-02-11
-        #
-        # TODO: file internal error report -- rjbs, 2016-02-11
-        last RV;
-      }
-    }
-  }
-
-  $ctx->state->_save_states;
-
-  $ctx->schema->txn_commit;
+    $ctx->state->_save_states;
+  });
 
   return \@results;
 }
