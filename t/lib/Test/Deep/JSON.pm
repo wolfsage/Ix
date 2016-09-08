a7591975da26fced5ce253b38000f3de36f30fba
Ricardo Signes <rjbs@cpan.org>
2016-09-08 10:31:18 -0400

    Test::Deep::JSON for testing JSON::Typist data

---
+++ t/lib/Test/Deep/JSON.pm
@@ -0,0 +1,42 @@
+use strict;
+use warnings;
+package Test::Deep::JSON;
+
+use Test::Deep ();
+
+use Exporter 'import';
+our @EXPORT = qw( jcmp_deeply jstr jnum jtrue jfalse );
+
+sub jcmp_deeply {
+  local $Test::Builder::Level = $Test::Builder::Level + 1;
+  local $Test::Deep::LeafWrapper = \&Test::Deep::str;
+  Test::Deep::cmp_deeply(@_);
+}
+
+sub jstr  { Test::Deep::all( Test::Deep::obj_isa('JSON::Typist::String'),
+                             Test::Deep::str($_[0])) }
+
+sub jnum  { Test::Deep::all( Test::Deep::obj_isa('JSON::Typist::Number'),
+                             Test::Deep::num($_[0])) }
+
+sub jtrue {
+  Test::Deep::all(
+    Test::Deep::any(
+      Test::Deep::obj_isa('JSON::XS::Boolean'),
+      Test::Deep::obj_isa('JSON::PP::Boolean'),
+    ),
+    bool(1),
+  );
+}
+
+sub jfalse {
+  Test::Deep::all(
+    Test::Deep::any(
+      Test::Deep::obj_isa('JSON::XS::Boolean'),
+      Test::Deep::obj_isa('JSON::PP::Boolean'),
+    ),
+    bool(0),
+  );
+}
+
+1;
