c4208f724a37fc7230a5e6259d0d3bdeb5b9d7f5
Matthew Horsfall <wolfsage@gmail.com>
2016-09-21 14:14:51 -0400

    Catch HTTP::Throwable exceptions more globally

--- lib/Ix/App.pm
+++ lib/Ix/App.pm
@@ -62,35 +62,22 @@ sub _build_psgi_app ($self) {
       ];
     }
 
-    my ($ctx, $exception);
+    my $ctx;
 
-    try {
+    my $res = try {
       $ctx = $self->processor->context_from_plack_request($req);
-    } catch {
-      $exception = $_;
-    };
+      $req->env->{'ix.ctx'} = $ctx;
 
-    if ($exception) {
-      unless ($exception->$_can('as_psgi')) {
-        die $exception;
-      }
+      state $transaction_number;
+      $transaction_number++;
+      $req->env->{'ix.transaction'} = {
+        guid => guid_string(),
+        time => Ix::DateTime->now,
+        seq  => $transaction_number,
+      };
 
-      return $exception->as_psgi;
-    }
-
-    $req->env->{'ix.ctx'} = $ctx;
-
-    state $transaction_number;
-    $transaction_number++;
-    $req->env->{'ix.transaction'} = {
-      guid => guid_string(),
-      time => Ix::DateTime->now,
-      seq  => $transaction_number,
-    };
-
-    my $res = eval {
-      my $calls;
-      unless (eval { $calls = $self->decode_json( $req->raw_body ); 1 }) {
+      my $calls = try { $self->decode_json( $req->raw_body ); };
+      unless ($calls) {
         return [
           400,
           [
@@ -114,13 +101,17 @@ sub _build_psgi_app ($self) {
         ],
         [ $json ],
       ];
-    };
+    } catch {
+      my $error = $_;
 
-    # TODO: handle HTTP::Throwable..? -- rjbs, 2016-08-12
-    unless ($res) {
-      my $error = $@;
-      my $guid  = $ctx->report_exception($error);
-      $res = [
+      # Let HTTP::Throwable pass through
+      if ($error->$_can('as_psgi')) {
+        return $error->as_psgi;
+      }
+
+      my $guid = $ctx ? $ctx->report_exception($error) : '';
+
+      return [
         500,
         [
           'Content-Type', 'application/json',
@@ -129,9 +120,9 @@ sub _build_psgi_app ($self) {
         ],
         [ qq<{"error":"internal","guid":"$guid"}> ],
       ];
-    }
+    };
 
-    my @error_guids = $ctx->logged_exception_guids;
+    my @error_guids = $ctx ? $ctx->logged_exception_guids : ();
 
     if (@error_guids or $self->log_all_transactions) {
       # We delete this so that when we stick the request into it, we don't
