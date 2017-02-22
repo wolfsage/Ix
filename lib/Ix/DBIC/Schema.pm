b9453a5f5228be53552b8c3278faaeaa7198c748
Ricardo Signes <rjbs@cpan.org>
2017-02-22 14:29:39 -0800

    expand a hard tab

--- lib/Ix/DBIC/Schema.pm
+++ lib/Ix/DBIC/Schema.pm
@@ -7,7 +7,7 @@ use parent 'DBIx::Class';
 use experimental qw(signatures postderef);
 
 sub ix_finalize ($self) {
-	my $source_reg = $self->source_registrations;
+  my $source_reg = $self->source_registrations;
   for my $moniker (keys %$source_reg) {
     my $rclass = $source_reg->{$moniker}->result_class;
     $rclass->ix_finalize if $rclass->can('ix_finalize');
